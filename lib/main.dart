98dce220a478db816c83993d26052ee8b59ad91e
AbhishekBansal <bansalabhishek7411@gmail.com>
2023-10-02 23:58:23 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
+    var h=MediaQuery.of(context).size.width;
+    var w=MediaQuery.of(context).size.height;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold()
+      home: Scaffold(
+        extendBody: false,
+        body: Padding(
+          padding:EdgeInsets.symmetric(horizontal: (w-w/1.1)/2),
+          child: Column(
+            children: [
+            const SizedBox(height: 100,),
+            Container(
+              alignment: Alignment.center,
+              width: w/1.1, height: h/3,color: Colors.pink,
+            padding:const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+              Container(height: h/8,width: w/2,color: Colors.grey,),
+              Container(height: h/8,width: w,color: Colors.green,),
+            ],),
+            )
+          ],),
+        )
+      )
     );
   }
 }
